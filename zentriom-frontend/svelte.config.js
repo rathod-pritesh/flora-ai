91d15f29faf3c14a22dca5711039a07be2119a52
Pritesh Rathod <rathodpritesh0712@gmail.com>
2026-07-13 13:02:23 +0530

    change in vercel adapter for frontend

--- zentriom-frontend/svelte.config.js
+++ zentriom-frontend/svelte.config.js
@@ -1,5 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-kit: {
-    adapter: adapter()
-}
+export default {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
+};
